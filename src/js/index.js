d28cf783c3a9aea294dbad4adf59804f7b64ffe9
Rhannie B <anuwebdesign@gmail.com>
2019-02-26 18:43:37 -0500

    add slack animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
 
+$(".btn-cta, .btn-cta-bottom").hover(function() {
+  $(".btn-cta img, .btn-cta-bottom img").toggleClass("imgHover");});
+
+  AOS.init({duration: 1500});
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
     scrollFunction()
@@ -14,7 +19,12 @@ window.onscroll = function() {
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
     document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; }
+    document.documentElement.scrollTop = 0; };
 
+  
 
         //  $("#myBtn").delay(1000).animate({"opacity": "1"}, 1500);
+
+        // <script>
+
+    //  </script>
